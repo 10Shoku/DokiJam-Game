10c7c8cbf1564f2b9573ed25e6f3863a1881903b
Chair <Chair.x86@gmail.com>
2025-08-04 16:31:56 +0200

    filepath patch

--- C-GDE/src/gdextension_interface.c
+++ C-GDE/src/gdextension_interface.c
@@ -2,3 +2,6 @@
 #include "Ch1Player.h"
 
 //...
+
+/* tmp against errors */
+GDExtensionBool DokiGDE_library_init(GDExtensionInterfaceGetProcAddress p_get_proc_address, GDExtensionClassLibraryPtr p_library, GDExtensionInitialization *r_initialization);
--- Main-Menu/main_menu.gd
+++ Main-Menu/main_menu.gd
@@ -1,10 +1,10 @@
 extends Control
 
 func _on_start_pressed() -> void:
-	get_tree().change_scene_to_file("res://chapter_1.tscn")
+	get_tree().change_scene_to_file("res://Chapter1/chapter_1.tscn")
 
 func _on_options_pressed() -> void:
-	get_tree().change_scene_to_file("res://options_menu.tscn")
+	get_tree().change_scene_to_file("res://Main-Menu/options_menu.tscn")
 
 func _on_quit_pressed() -> void:
 	get_tree().quit()
