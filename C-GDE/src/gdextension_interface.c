22de1a520638fb0376a3e3e5b6588d9592abf10c
Chair <Chair.x86@gmail.com>
2025-08-04 16:38:46 +0200

    gde-lib-init-error patch

--- C-GDE/src/gdextension_interface.c
+++ C-GDE/src/gdextension_interface.c
@@ -4,4 +4,6 @@
 //...
 
 /* tmp against errors */
-GDExtensionBool DokiGDE_library_init(GDExtensionInterfaceGetProcAddress p_get_proc_address, GDExtensionClassLibraryPtr p_library, GDExtensionInitialization *r_initialization);
+GDExtensionBool DokiGDE_library_init(GDExtensionInterfaceGetProcAddress p_get_proc_address, GDExtensionClassLibraryPtr p_library, GDExtensionInitialization *r_initialization) {
+    return true;
+}
